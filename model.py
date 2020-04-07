8022d3fdaaf81d8460423d4818c955623cfa5ad1
Andrew <andrew_dadario@hotmail.com>
2020-04-07 03:15:38 -0300

    Update model.py
    Changed image sizes to 256 pixels

--- model.py
+++ model.py
@@ -21,7 +21,7 @@ import numpy as np
 w_bifpns = [64, 88, 112, 160, 224, 288, 384]
 d_bifpns = [3, 4, 5, 6, 7, 7, 8]
 d_heads = [3, 3, 3, 4, 4, 4, 5]
-image_sizes = [512, 640, 768, 896, 1024, 1280, 1408]
+image_sizes = [256, 256, 256, 256, 256, 256, 256]#[512, 640, 768, 896, 1024, 1280, 1408]
 backbones = [EfficientNetB0, EfficientNetB1, EfficientNetB2,
              EfficientNetB3, EfficientNetB4, EfficientNetB5, EfficientNetB6]
 
